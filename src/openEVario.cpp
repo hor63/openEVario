5dafa9f8dad52c585973fd45a3e1bc0d5aa5739a
hor <horstmannkai@hotmail.com>
2015-12-06 21:08:24 +0100

    Initial commit of the build frame as Eclipse autotools C++ project

---
+++ src/openEVario.cpp
@@ -0,0 +1,60 @@
+/*
+ ============================================================================
+ Name        : openEVario.cpp
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <random>
+#include <iostream>
+
+using namespace std;
+
+mt19937 randomGenerator;
+
+int main(void) {
+  double U1,U2,V1,V2,S,polarFactor,X,Y	;
+  mt19937::result_type min;
+  double range;
+  double sqrSum = 0.0,sum = 0.0,absSum =0.0;
+int i;
+
+	min = randomGenerator.min();
+	range = double(randomGenerator.max()-randomGenerator.min());
+
+	cout << "Hello World" << endl; /* prints Hello World */
+
+	cout << " min of randomGenerator = " << min << endl;
+	cout << " range of randomGenerator = " << range << endl;
+
+	for (i=0; i<10000000;i++) {
+	    do {
+		U1 = double((randomGenerator()) - min) / range;
+		U2 = double((randomGenerator()) - min) / range;
+		V1=2.0 * U1 -1.0;            /* V1=[-1,1] */
+		V2=2.0 * U2 - 1.0;           /* V2=[-1,1] */
+		S=V1 * V1 + V2 * V2;
+	    }  while (S >=1.0);
+	    polarFactor = sqrt(-2.0 * log(S) / S);
+	    X = polarFactor * V1;
+	    Y = polarFactor * V2;
+	    sum += X + Y;
+	    sqrSum += X*X + Y*Y;
+	    absSum += fabs(X) + fabs(Y);
+
+	}
+
+
+	cout << "sum = " << sum << " Mean of " << i*2 << " samples is " << sum / double(i*2)  << endl;
+	cout << "Average deviation of " << i*2 << " samples is " << absSum / double(i*2)  << endl;
+	cout << "the variance of " << i*2 << " samples is " << sqrSum / double(i*2) << endl;
+	cout << "the standard deviation is " << sqrt (sqrSum / double(i*2)) << endl;
+	cout << "the sample variance of " << i*2 << " samples is " << sqrSum / double(i*2-1) << endl;
+	cout << "the sample standard deviation is " << sqrt (sqrSum / double(i*2-1)) << endl;
+
+
+	return 0;
+}
