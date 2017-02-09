128b9003585a60367140bc30302865e4db3802f0
Kai Horstmann <horstmannkai@hotmail.com>
2017-02-09 23:19:13 +0100

    Adjust precision of expected result to 1e-5

--- test/src/RotationMatrix_test.cpp
+++ test/src/RotationMatrix_test.cpp
@@ -57,15 +57,15 @@ TEST_F(RotationMatrixTest, CoefficientTest) {
 
 				EXPECT_LE ( fabs(double(glo2Plane(0,0)) - cos(thetaRad) * cos(psiRad)) , 0.00001 );
 				EXPECT_LE ( fabs(double(glo2Plane(0,1)) - (cos(thetaRad) * sin(psiRad))) , 0.00001 );
-				EXPECT_LE ( fabs(double(glo2Plane(0,2)) + sin(thetaRad)), 0.001);
+				EXPECT_LE ( fabs(double(glo2Plane(0,2)) + sin(thetaRad)), 0.00001);
 
 				EXPECT_LE ( fabs(double(glo2Plane(1,0)) - (sin(phiRad) * sin(thetaRad) * cos(psiRad) - cos(phiRad) * sin(psiRad))),0.00001);
 				EXPECT_LE ( fabs(double(glo2Plane(1,1)) - (sin(phiRad) * sin(thetaRad) * sin(psiRad) + cos(phiRad) * cos(psiRad))),0.00001);
-				EXPECT_LE ( fabs(double(glo2Plane(1,2)) - (sin(phiRad) * cos(thetaRad))),0.0001);
+				EXPECT_LE ( fabs(double(glo2Plane(1,2)) - (sin(phiRad) * cos(thetaRad))),0.00001);
 
 				EXPECT_LE ( fabs(double(glo2Plane(2,0)) - (cos(phiRad) * sin(thetaRad) * cos(psiRad) + sin(phiRad) * sin(psiRad))),0.00001);
 				EXPECT_LE ( fabs(double(glo2Plane(2,1)) - (cos(phiRad) * sin(thetaRad) * sin(psiRad) - sin(phiRad) * cos(psiRad))),0.00001);
-				EXPECT_LE ( fabs(double(glo2Plane(2,2)) - (cos(phiRad) * cos(thetaRad))),0.0001);
+				EXPECT_LE ( fabs(double(glo2Plane(2,2)) - (cos(phiRad) * cos(thetaRad))),0.00001);
 
 			}
 		}
@@ -111,9 +111,9 @@ TEST_F(RotationMatrixTest, VectorTest) {
 
 				// convert the plane vector back to the world coordinates, and compare with the original
 				rotMatrix.calcPlaneVectorToWorldVector(vect1,vect2);
-				EXPECT_LE (fabs(orgVect(0)-vect2(0)),0.0000001);
-				EXPECT_LE (fabs(orgVect(1)-vect2(1)),0.0000001);
-				EXPECT_LE (fabs(orgVect(2)-vect2(2)),0.0000001);
+				EXPECT_LE (fabs(orgVect(0)-vect2(0)),0.000001);
+				EXPECT_LE (fabs(orgVect(1)-vect2(1)),0.000001);
+				EXPECT_LE (fabs(orgVect(2)-vect2(2)),0.000001);
 
 			}
 		}
