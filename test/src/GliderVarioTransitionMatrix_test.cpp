622a0f7f613325bb2fc9ac6631f9d4523146b383
Kai Horstmann <horstmannkai@hotmail.com>
2017-02-09 23:20:31 +0100

    Implement first test for transition matrix: constant gravity

--- test/src/GliderVarioTransitionMatrix_test.cpp
+++ test/src/GliderVarioTransitionMatrix_test.cpp
@@ -23,5 +23,43 @@
  *
  */
 
+#include "gtest/gtest.h"
 #include "GliderVarioTransitionMatrix.h"
+
+using namespace openEV;
+
+class TransitionMatrixTest :public ::testing::Test {
+public:
+
+
+	openEV::GliderVarioTransitionMatrix transMatrix;
+	openEV::GliderVarioStatus st1,st2;
+};
+
+TEST_F(TransitionMatrixTest, Gravity) {
+
+	// Test the result for a given combination of input values
+	// and a number of time differences
+	// input values are: Gravity
+	for (FloatType t = 0.01f; t<=1.0f; t+=0.1f  ) {
+		for (FloatType g = 9.0f ; g <= 10.f; g+=0.01f) {
+			FloatType tmp1;
+			st1.gravity = g;
+
+			transMatrix.updateStatus(st1,st2,t);
+
+			// Now check the result
+			EXPECT_EQ (st1.gravity,st2.gravity);
+
+			// check the increment with the transition matrix
+			tmp1 = st2.gravity + 0.01f * transMatrix.getTransitionMatrix().coeff(st1.STATUS_IND_GRAVITY,st1.STATUS_IND_GRAVITY);
+			st1.gravity = g + 0.01f;
+			transMatrix.updateStatus(st1,st2,t);
+			EXPECT_EQ (tmp1,st2.gravity);
+
+
+		}
+	}
+
+}
 
