61349ebec9c38e22da6f46aa986f4dfd383b96b0
Kai Horstmann <horstmannkai@hotmail.com>
2017-10-06 14:13:38 +0200

    Change macro names DLL_... to OV_DLL_... to avoid collisions with command line defined from libtool

--- src/OEVCommon.h
+++ src/OEVCommon.h
@@ -29,48 +29,48 @@
 #define OEVCOMMON_H_
 
 /**
- * Define DLL_IMPORT, DLL_EXPORT, and DLL_LOCAL for Windows and Linux (ELF) ports of gcc and non-gcc compilers
+ * Define OV_DLL_IMPORT, OV_DLL_EXPORT, and OV_DLL_LOCAL for Windows and Linux (ELF) ports of gcc and non-gcc compilers
  *
  * The macro definitions are highly inspired from the <a href="https://gcc.gnu.org/wiki/Visibility">GCC Wiki: Visibility</a>
  */
 #if defined _WIN32 || defined __CYGWIN__
     #ifdef __GNUC__
-      #define DLL_EXPORT __attribute__ ((dllexport))
-      #define DLL_IMPORT __attribute__ ((dllimport))
+      #define OV_DLL_EXPORT __attribute__ ((dllexport))
+      #define OV_DLL_IMPORT __attribute__ ((dllimport))
     #else
-      #define DLL_EXPORT __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
-      #define DLL_IMPORT __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
+      #define OV_DLL_EXPORT __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+      #define OV_DLL_IMPORT __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
     #endif
     #ifdef __GNUC__
     #else
     #endif
-  #define DLL_LOCAL
+  #define OV_DLL_LOCAL
 #else
   #if __GNUC__ >= 4
-    #define DLL_EXPORT __attribute__ ((visibility ("default")))
-    #define DLL_LOCAL  __attribute__ ((visibility ("hidden")))
+    #define OV_DLL_EXPORT __attribute__ ((visibility ("default")))
+    #define OV_DLL_LOCAL  __attribute__ ((visibility ("hidden")))
   #else
-    #define DLL_EXPORT
-    #define DLL_LOCAL
+    #define OV_DLL_EXPORT
+    #define OV_DLL_LOCAL
   #endif
-  #define DLL_IMPORT
+  #define OV_DLL_IMPORT
 #endif
 
 #if defined (BUILDING_OEV_KALMAN)
-  #define OEV_PUBLIC DLL_EXPORT
-  #define OEV_LOCAL  DLL_LOCAL
+  #define OEV_PUBLIC OV_DLL_EXPORT
+  #define OEV_LOCAL  OV_DLL_LOCAL
 #else /* BUILDING_OEV_KALMAN */
-  #define OEV_PUBLIC DLL_IMPORT
-  #define OEV_LOCAL  DLL_LOCAL
+  #define OEV_PUBLIC OV_DLL_IMPORT
+  #define OEV_LOCAL  OV_DLL_LOCAL
 #endif /* BUILDING_OEV_KALMAN */
 
 
 #if defined (BUILDING_OEV_UTILS)
-  #define OEV_UTILS_PUBLIC DLL_EXPORT
-  #define OEV_UTILS_LOCAL  DLL_LOCAL
+  #define OEV_UTILS_PUBLIC OV_DLL_EXPORT
+  #define OEV_UTILS_LOCAL  OV_DLL_LOCAL
 #else /* BUILDING_OEV_KALMAN */
-  #define OEV_UTILS_PUBLIC DLL_IMPORT
-  #define OEV_UTILS_LOCAL  DLL_LOCAL
+  #define OEV_UTILS_PUBLIC OV_DLL_IMPORT
+  #define OEV_UTILS_LOCAL  OV_DLL_LOCAL
 #endif /* BUILDING_OEV_UTILS */
 
 namespace openEV {
