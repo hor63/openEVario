0764a1b7037119d80d27fd6683b84d90d1573173
Kai Horstmann <horstmannkai@hotmail.com>
2018-02-05 00:02:07 +0100

    Start working on the driver list

---
+++ src/main/GliderVarioDriverList.h
@@ -0,0 +1,60 @@
+/*
+ * GliderVarioDriverList.h
+ *
+ *  Created on: 04.02.2018
+ *      Author: hor
+ *
+ *   This file is part of openEVario, an electronic variometer for glider planes
+ *   Copyright (C) 2016  Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef MAIN_GLIDERVARIODRIVERLIST_H_
+#define MAIN_GLIDERVARIODRIVERLIST_H_
+
+#include <memory>
+#include <map>
+
+
+namespace openEV {
+
+/** \brief Administration of the driver list
+ *
+ * Administration, loading, storing, iteration through, and access to the list of sensor drivers
+ *
+ */
+class GliderVarioDriverList {
+public:
+	GliderVarioDriverList() {
+		// TODO Auto-generated constructor stub
+
+	}
+	virtual ~GliderVarioDriverList();
+
+
+
+protected:
+
+};
+
+
+
+
+
+} /* namespace openEV */
+
+#endif /* MAIN_GLIDERVARIODRIVERLIST_H_ */
