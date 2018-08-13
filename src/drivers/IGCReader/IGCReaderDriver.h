39b7b0c8eda2f27a7311040f029e01650c432085
Kai Horstmann <horstmannkai@hotmail.com>
2018-08-13 19:53:27 +0200

    Add IGC reader driver as a test/simulation driver

---
+++ src/drivers/IGCReader/IGCReaderDriver.h
@@ -0,0 +1,37 @@
+/*
+ * IGCReaderDriver.h
+ *
+ *  Created on: Sep 29, 2017
+ *      Author: hor
+ *
+ *  Internal definitions of the simulation driver
+ *
+ *   This file is part of openEVario, an electronic variometer for glider planes
+ *   Copyright (C) 2016  Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef IGCREADERDRIVER_H_
+#define IGCREADERDRIVER_H_
+
+#include "OEVCommon.h"
+
+// included here because the defines above are used in the include
+#include "drivers/sensorDriver.h"
+
+#endif /* IGCREADERDRIVER_H_ */
+
---
+++ src/drivers/IGCReader/IGCReaderMain.cpp
@@ -0,0 +1,74 @@
+/* Simulation driver main module
+ *
+ * simulMain.cpp
+ *
+ *  Created on: Sep 24, 2017
+ *      Author: hor
+ *
+ *   This file is part of openEVario, an electronic variometer for glider planes
+ *   Copyright (C) 2017  Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include "kalman/GliderVarioStatus.h"
+#include "IGCReaderDriver.h"
+#include "drivers/sensorDriver.h"
+
+using namespace openEV;
+
+static log4cxx::LoggerPtr logger = 0;
+
+static void initLogger() {
+	if (!logger) {
+		logger = log4cxx::Logger::getLogger("openEV.Drivers.Simul");
+	}
+}
+
+extern "C" {
+/** \brief Do whatever initialization is required
+ *
+ * This function must tolerate to be called multiple times without adverse effects.
+ *
+ */
+void OEV_DRIVER_PUBLIC driverInit(void) {
+
+	initLogger();
+
+	LOG4CXX_INFO(logger,"Initialize the simulation driver");
+
+}
+
+
+/** \brief Return a pointer to a driver object. The object must be created with the operator 'new'.
+ *
+ * @return Pointer to the driver object
+ */
+openEV::GliderVarioDriverBasePtr OEV_DRIVER_PUBLIC getDriver() {
+
+	initLogger();
+
+	LOG4CXX_INFO(logger,"getDriver");
+
+	return 0;
+}
+
+
+}
