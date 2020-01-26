6e4caa0010ec0b989e602b7ee5704c0e10a5f5a3
Kai Horstmann <horstmannkai@hotmail.com>
2020-01-26 23:36:22 +0100

    src/util/io/TCPPort.*: Add copyright to sources, add TCP includes depending on config.h

--- src/util/io/TCPPort.cpp
+++ src/util/io/TCPPort.cpp
@@ -3,7 +3,40 @@
  *
  *  Created on: Dec 23, 2019
  *      Author: hor
+ *
+ *   This file is part of openEVario, an electronic variometer for glider planes
+ *   Copyright (C) 2019  Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
  */
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#if HAVE_SYS_TYPES_H
+#	include <sys/types.h>
+#endif
+
+#if HAVE_SYS_SOCKET_H
+#	include <sys/socket.h>
+#endif
+
+#if HAVE_NETDB_H
+#	include <netdb.h>
+#endif
 
 #include "util/io/TCPPort.h"
 
