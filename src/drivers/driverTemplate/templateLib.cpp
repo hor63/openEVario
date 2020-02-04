ee97a6ff317f6f1ccc92d2b041ba1e5d2b8f86bf
Kai Horstmann <horstmannkai@hotmail.com>
2020-02-04 21:50:12 +0100

    Add a driver template with an empty skeleton for a new driver

---
+++ src/drivers/driverTemplate/templateLib.cpp
@@ -0,0 +1,79 @@
+/*
+ * templateLib.cpp
+ *
+ *  Created on: Feb 04, 2020
+ *      Author: kai_horstmann
+ *
+ *   This file is part of openEVario, an electronic variometer for glider planes
+ *   Copyright (C) 2020  Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include "OEVCommon.h"
+
+
+#include "templateLib.h"
+#include "templateDriver.h"
+
+namespace openEV {
+
+static std::string const templateDriverName = "template";
+static std::string const templateDriverLibName = "templateLib";
+
+templateLib templateLib::theOneAndOnly;
+
+
+static GliderVarioDriverBase* getNewtemplateInstance (
+	    char const *driverName,
+		char const *description,
+		char const *instanceName) {
+
+	if (templateDriverName.compare(driverName)) {
+		return 0;
+	}
+	return new templateDriver (driverName,description,instanceName);
+}
+
+templateLib::templateLib()
+	: GliderVarioDriverLibBase{
+		templateDriverLibName.c_str(),
+		"template for new driver library"}
+{
+
+
+}
+
+
+templateLib::~templateLib() {
+
+}
+
+void templateLib::addDrivers(GliderVarioDriverList &gliderVarioDriverList) {
+	GliderVarioDriverList::DriverListItem listItem {
+		templateDriverName,
+		"template for new driver",
+		getNewtemplateInstance};
+
+	gliderVarioDriverList.addDriver(listItem);
+}
+
+
+} /* namespace openEV */
---
+++ src/drivers/driverTemplate/templateMain.cpp
@@ -0,0 +1,78 @@
+/*
+ * templateMain.cpp
+ *
+ *  Created on: Feb 04, 2020
+ *      Author: kai_horstmann
+ *
+ *   This file is part of openEVario, an electronic variometer for glider planes
+ *   Copyright (C) 2020  Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include "kalman/GliderVarioStatus.h"
+#include "templateLib.h"
+#include "templateDriver.h"
+#include "drivers/sensorDriver.h"
+
+using namespace openEV;
+
+#if defined HAVE_LOG4CXX_H
+static log4cxx::LoggerPtr logger = 0;
+
+static void initLogger() {
+	if (!logger) {
+		logger = log4cxx::Logger::getLogger("openEV.Drivers.template");
+	}
+}
+#endif /* defined HAVE_LOG4CXX_H */
+
+extern "C" {
+
+void OEV_DRIVER_PUBLIC driverLibInit(void) {
+
+	static bool initialized = false;
+
+	if (initialized) {
+		return;
+	}
+
+#if defined HAVE_LOG4CXX_H
+	initLogger();
+#endif
+
+	LOG4CXX_INFO(logger,"Initialize the template driver library");
+
+	initialized = true;
+
+}
+
+
+openEV::GliderVarioDriverLibBasePtr OEV_DRIVER_PUBLIC getDriverLib() {
+
+	driverLibInit();
+
+	LOG4CXX_INFO(logger,"template: getDriverLib");
+
+	return &templateLib::theOneAndOnly;
+}
+
+
+}
