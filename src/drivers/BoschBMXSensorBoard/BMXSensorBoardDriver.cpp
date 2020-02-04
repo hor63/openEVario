2cef247e8f29f3ed6a8d5c111ee726e721428507
Kai Horstmann <horstmannkai@hotmail.com>
2020-02-04 21:28:14 +0100

    Add Bosch BMX IMU driver skeleton.
    The Bosch BMX driver is being connected by the hovImuBoard sonsor board
    and supports the BMX160 IMU.

---
+++ src/drivers/BoschBMXSensorBoard/BMXSensorBoardDriver.cpp
@@ -0,0 +1,127 @@
+/*
+ * BMXSensorBoardDriver.cpp
+ *
+ *  Created on: Feb 04, 2020
+ *      Author: kai_horstmann
+ *
+ *   This file is part of openEVario, an electronic variometer for glider planes
+ *   Copyright (C) 2020  Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <fstream>
+
+#include "BMXSensorBoardDriver.h"
+#include "kalman/GliderVarioTransitionMatrix.h"
+#include "kalman/GliderVarioMeasurementUpdater.h"
+
+
+#if defined HAVE_LOG4CXX_H
+static log4cxx::LoggerPtr logger = 0;
+
+static inline void initLogger() {
+	if (!logger) {
+		logger = log4cxx::Logger::getLogger("openEV.Drivers.BMXSensorBoard");
+	}
+}
+
+#endif
+
+namespace openEV {
+
+BMXSensorBoardDriver::BMXSensorBoardDriver(
+	    char const *driverName,
+		char const *description,
+		char const *instanceName
+		)
+: GliderVarioDriverBase {driverName,description,instanceName,BMXSensorBoardLib::theOneAndOnly}
+{
+
+#if defined HAVE_LOG4CXX_H
+	initLogger();
+#endif /* HAVE_LOG4CXX_H */
+
+	setSensorCapability(GPS_LATITUDE	);
+	setSensorCapability(GPS_LONGITUDE	);
+	setSensorCapability(GPS_ALTITUDE_MSL);
+	setSensorCapability(STATIC_PRESSURE	);
+	setSensorCapability(RUN_IDLE_LOOP	);
+
+}
+
+
+BMXSensorBoardDriver::~BMXSensorBoardDriver() {
+
+	/// todo fill me
+
+}
+
+
+void BMXSensorBoardDriver::driverInit() {
+
+}
+
+void BMXSensorBoardDriver::readConfiguration (Properties4CXX::Properties const &configuration) {
+
+	/// todo fill me
+
+}
+
+void BMXSensorBoardDriver::initializeStatus(
+		GliderVarioStatus &varioStatus,
+		GliderVarioMainPriv &varioMain) {
+
+	/// todo fill me
+
+}
+
+void BMXSensorBoardDriver::start(GliderVarioMainPriv &varioMain) {
+
+	GliderVarioDriverBase::start(varioMain);
+
+}
+
+void BMXSensorBoardDriver::suspend() {
+
+	/// todo fill me
+
+}
+
+void BMXSensorBoardDriver::resume() {
+
+	/// todo fill me
+
+}
+
+void BMXSensorBoardDriver::updateKalmanStatus (GliderVarioStatus &varioStatus) {
+
+	/// todo fill me
+
+}
+
+
+void BMXSensorBoardDriver::driverThreadFunction() {
+
+	/// todo fill me
+}
+
+
+} /* namespace openEV */
---
+++ src/drivers/BoschBMXSensorBoard/BMXSensorBoardLib.cpp
@@ -0,0 +1,79 @@
+/*
+ * BMXSensorBoardLib.cpp
+ *
+ *  Created on: Feb 04, 2020
+ *      Author: kai_horstmann
+ *
+ *   This file is part of openEVario, an electronic variometer for glider planes
+ *   Copyright (C) 2020  Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include "OEVCommon.h"
+
+
+#include "BMXSensorBoardLib.h"
+#include "BMXSensorBoardDriver.h"
+
+namespace openEV {
+
+static std::string const BMXSensorBoardDriverName = "BMXSensorBoard";
+static std::string const BMXSensorBoardDriverLibName = "BMXSensorBoardLib";
+
+BMXSensorBoardLib BMXSensorBoardLib::theOneAndOnly;
+
+
+static GliderVarioDriverBase* getNewBMXSensorBoardInstance (
+	    char const *driverName,
+		char const *description,
+		char const *instanceName) {
+
+	if (BMXSensorBoardDriverName.compare(driverName)) {
+		return 0;
+	}
+	return new BMXSensorBoardDriver (driverName,description,instanceName);
+}
+
+BMXSensorBoardLib::BMXSensorBoardLib()
+	: GliderVarioDriverLibBase{
+		BMXSensorBoardDriverLibName.c_str(),
+		"Driver library for Bosch BMX IMUs on the horImuBoard sensor board"}
+{
+
+
+}
+
+
+BMXSensorBoardLib::~BMXSensorBoardLib() {
+
+}
+
+void BMXSensorBoardLib::addDrivers(GliderVarioDriverList &gliderVarioDriverList) {
+	GliderVarioDriverList::DriverListItem listItem {
+		BMXSensorBoardDriverName,
+		"Driver for Bosch BMX160 IMUs on the horImuBoard sensor board",
+		getNewBMXSensorBoardInstance};
+
+	gliderVarioDriverList.addDriver(listItem);
+}
+
+
+} /* namespace openEV */
