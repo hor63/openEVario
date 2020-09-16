f09998874128a8c25bf4db38b33d2e48af0dd6e4
kai_horstmann <kai_horstmann@werum2920.werum.net>
2020-09-16 23:30:39 +0200

    NMEA0813: Implement processSentence() code

--- src/drivers/NmeaGPS/NMEA0813.cpp
+++ src/drivers/NmeaGPS/NMEA0813.cpp
@@ -140,6 +140,89 @@ void NMEA0813::processSensorData(const uint8_t *data, uint32_t dataLen) {
 }
 
 void NMEA0813::processSentence() {
+
+	uint8_t checksum = 0;
+
+	for (uint32_t i=0; i<maxLenSentence && currSentence.buf[i] != 0; i++) {
+
+		// The last first: Checksum check, and the end of the message.
+		if (currSentence.buf[i] == '*') {
+
+			// Terminate the previous field
+			currSentence.buf[i] = 0;
+			if (isxdigit(currSentence.buf[i + 1]) &&
+					isxdigit(currSentence.buf[i + 2]) &&
+					currSentence.buf[i + 3] == 0) {
+				auto msgChkSum = strtoul((char const*)(&currSentence.buf[i + 1]),nullptr,16);
+				if (msgChkSum == checksum){
+					LOG4CXX_DEBUG(logger, "Checksum \"" << &currSentence.buf[i + 1] << "\" matches.");
+				}
+			} else {
+				LOG4CXX_WARN(logger,"Read invalid checksum section \""
+						<< &currSentence.buf[i] << "\". Discard sentence.");
+				return;
+			}
+			// End of story in any case.
+			break;
+		}
+
+		// All characters of the sentence except the '*' at the end is subject to the checksum.
+		// The $ character is already stripped away.
+		checksum ^= currSentence.buf[i];
+
+		if (currSentence.buf[i] == ',') {
+			// Field separator
+			// Make it a \0 string terminator
+			currSentence.buf[i] = 0;
+
+			if (currSentence.numFields < maxNumFields) {
+			// Store the start of the next field in the sentence
+			currSentence.fields[currSentence.numFields] = &currSentence.buf[i+1];
+			currSentence.numFields++;
+			} else {
+				LOG4CXX_WARN(logger,"NMEA: Number fields " << currSentence.numFields << " << exceeds the field pointer array."
+						<< " Remainder of message is: \"" << &currSentence.buf[i+1] << "\"");
+			}
+		}
+
+	}
+
+	// Process the type of sentence, and talker ID.
+	if (currSentence.buf[0] == 'P') {
+		// Proprietary message Talker ID is one character.
+		currSentence.TalkerID[0] = 'P';
+		currSentence.TalkerID[1] = 0;
+	} else {
+		// A regular NMEA message. Talker ID is expected to be 2 char.
+		// But you never can be sure. I will check that too.
+		currSentence.TalkerID[0] = currSentence.buf[0];
+		currSentence.TalkerID[1] = currSentence.buf[1];
+		currSentence.TalkerID[2] = 0;
+
+		// Prevent spill-over of sentence type into the next field in case
+		// that the preamble is only one character, and not 'P'.
+		// Illegal, but I want to avoid any accidents
+		if (currSentence.buf[1] != 0) {
+			currSentence.SentenceType = &currSentence.buf[2];
+		} else {
+			// currSentence.buf[1] is already \0. Therefore the sentence type is also emtpy.
+			currSentence.SentenceType = &currSentence.buf[1];
+		}
+	}
+
+#if defined HAVE_LOG4CXX_H
+	if (logger->isDebugEnabled()) {
+		std::ostringstream txt;
+		txt << "NMEA sentence parsed: \nTalker ID: \"" << currSentence.TalkerID
+				<< "\", \nsentence type: \"" << currSentence.SentenceType
+				<< "\", \nnumber fields: " << currSentence.numFields;
+		for (uint32_t i = 0; i < currSentence.numFields; i++) {
+			txt << "\n	fields[" << i << "] = \"" << currSentence.fields[i] << "\"";
+		}
+		LOG4CXX_DEBUG(logger,txt.str());
+	}
+#endif // #if defined HAVE_LOG4CXX_H
+
 }
 
 } /* namespace openEV */
