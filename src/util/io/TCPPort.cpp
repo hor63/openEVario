45b0c6f7e22833a3c03ef71b0e2bca0fe757a6c0
Kai Horstmann <horstmannkai@hotmail.com>
2024-04-25 22:37:55 +0200

    TCPPort::openInternal(): Rework error handling, and remove memory leak
    Simplify the structure of the method with error handling.
    Manage dynamic memory returned by getaddrinfo with the
    struct AddrInfoManagerTCP. Its destructor safely releases the memory
    allocated by getaddrinfo(), also in all cases where the method is left
    via the multiple thrown exceptions.
    
    Signed-off-by: Kai Horstmann <horstmannkai@hotmail.com>

--- src/util/io/TCPPort.cpp
+++ src/util/io/TCPPort.cpp
@@ -134,76 +134,54 @@ std::string currPropertyName;
 
 }
 
+/** \brief Manages the dynamically allocated memory from getaddrinfo
+ *
+ * Use an object of this struct to release the dynamically allocated memory
+ * in all circumstances, including the numerous cases when an exception is thrown.
+ */
+struct AddrInfoManagerTCP final {
+	struct addrinfo addrHint;
+	struct addrinfo *addr = nullptr;
+
+	AddrInfoManagerTCP () {
+		::memset (&addrHint,0,sizeof(addrHint));
+	}
+
+	~AddrInfoManagerTCP () {
+		if (addr != nullptr) {
+			::freeaddrinfo(addr);
+		}
+	}
+};
+
 void TCPPort::openInternal() {
 
 	int sock = -1;
 	int rc;
-	struct addrinfo addrHint;
-	struct addrinfo *addr = NULL;
-
-	::memset (&addrHint,0,sizeof(addrHint));
+	AddrInfoManagerTCP addrMgr;
 
 	// Allow TCP as well as TCP V6; Do not insist on TCP V4
-	// addrHint.ai_family = AF_INET;
+	// addrMgr.addrHint.ai_family = AF_INET;
 
 	// But insist on TCP; do not allow UDP
-	addrHint.ai_socktype = SOCK_STREAM;
+	addrMgr.addrHint.ai_socktype = SOCK_STREAM;
 
-	rc = ::getaddrinfo(tcpAddr.c_str(),tcpPort.c_str(),&addrHint,&addr);
+	rc = ::getaddrinfo(tcpAddr.c_str(),tcpPort.c_str(),&addrMgr.addrHint,&addrMgr.addr);
 
-	if (rc == 0) {
-		struct addrinfo *ad = addr;
+	if (rc != 0 || addrMgr.addr == nullptr) {
+		std::string str;
 
-		while (ad) {
-
-			auto sockType = ad->ai_socktype;
-
-			if (!isBlocking()) {
-				sockType |= SOCK_NONBLOCK;
-			}
-
-			sock = ::socket(ad->ai_family,sockType,ad->ai_protocol);
-
-			if (sock == -1) {
-				rc = errno;
-				auto str = fmt::format(_(
-						"{0}: Error creating a {1} socket for port \"{2}\". errno = {3}: {4}"),
-						__PRETTY_FUNCTION__,"TCP", getPortName(), rc, strerror(rc));
-				LOG4CXX_ERROR(logger,str);
-				throw GliderVarioPortOpenException (
-						__FILE__,
-						__LINE__,
-						str.c_str(),
-						rc);
-			}
-
-			if (sock != -1) {
-				rc = ::connect(sock,ad->ai_addr,ad->ai_addrlen);
-				if (rc == -1) {
-					rc = errno;
-					auto str = fmt::format(_(
-							"{0}: Error connect TCP socket to host \"{1}\" on TCP port \"{2}\" for I/O port \"{3}\". errno = {4}: {5}"),
-							__PRETTY_FUNCTION__, tcpAddr, tcpPort, getPortName(), rc, strerror(rc));
-					LOG4CXX_ERROR(logger,str);
-					::close (sock);
-					sock = -1;
-					throw GliderVarioPortOpenException (
-							__FILE__,
-							__LINE__,
-							str.c_str(),
-							rc);
-				}
-			}
-
-			if (sock != -1) {
-				break;
-			}
-			ad = ad->ai_next;
-		}
-	} else {
-		auto str = fmt::format(_(
+		if (rc != 0) {
+			str = fmt::format(_(
 				"{0}: Error resolving host \"{1}\" and/or TCP port \"{2}\" for I/O port \"{3}\". Error code = {4}: {5}"),
 				__PRETTY_FUNCTION__, tcpAddr, tcpPort, getPortName(), rc, gai_strerror(rc));
+		} else {
+			str = fmt::format(_(
+				"{0}: Error resolving host \"{1}\" and/or TCP port \"{2}\" for I/O port \"{3}\". "
+				"getaddrinfo() returned is 0 but *_pai is NULL."
+					),
+				__PRETTY_FUNCTION__, tcpAddr, tcpPort, getPortName());
+		}
 
 		LOG4CXX_ERROR(logger,str);
 		throw GliderVarioPortOpenException (
@@ -213,9 +191,43 @@ void TCPPort::openInternal() {
 				rc);
 	}
 
-	if (addr != NULL) {
-		::freeaddrinfo(addr);
+	auto sockType = addrMgr.addr->ai_socktype;
+
+	if (!isBlocking()) {
+		sockType |= SOCK_NONBLOCK;
 	}
+
+	sock = ::socket(addrMgr.addr->ai_family,sockType,addrMgr.addr->ai_protocol);
+
+	if (sock == -1) {
+		rc = errno;
+		auto str = fmt::format(_(
+				"{0}: Error creating a {1} socket for port \"{2}\". errno = {3}: {4}"),
+				__PRETTY_FUNCTION__,"TCP", getPortName(), rc, strerror(rc));
+		LOG4CXX_ERROR(logger,str);
+		throw GliderVarioPortOpenException (
+				__FILE__,
+				__LINE__,
+				str.c_str(),
+				rc);
+	}
+
+	rc = ::connect(sock,addrMgr.addr->ai_addr,addrMgr.addr->ai_addrlen);
+	if (rc == -1) {
+		rc = errno;
+		auto str = fmt::format(_(
+				"{0}: Error connect TCP socket to host \"{1}\" on TCP port \"{2}\" for I/O port \"{3}\". errno = {4}: {5}"),
+				__PRETTY_FUNCTION__, tcpAddr, tcpPort, getPortName(), rc, strerror(rc));
+		LOG4CXX_ERROR(logger,str);
+		::close (sock);
+		sock = -1;
+		throw GliderVarioPortOpenException (
+				__FILE__,
+				__LINE__,
+				str.c_str(),
+				rc);
+	}
+
 
 	{
 		DeviceHandleAccess devAcc(*this);
@@ -224,28 +236,24 @@ void TCPPort::openInternal() {
 
 	LOG4CXX_INFO(logger,fmt::format(_("Port \"{0}\": Connected to host {1} on TCP port {2}.")));
 
-	if (sock != -1) {
-		int flag = 1;
+	int flag = 1;
 #if TCP_NODELAY
-		rc = ::setsockopt(sock,IPPROTO_TCP,TCP_NODELAY,&flag, sizeof(flag));
-		if (rc == -1) {
-			rc = errno;
-			LOG4CXX_WARN(logger,fmt::format(_("Open I/O port \"{0}\": setsockopt {1} error: errno = {2} : {3}"),
-					getPortName(),"TCP_NODELAY",rc,strerror(rc)));
-		}
+	rc = ::setsockopt(sock,IPPROTO_TCP,TCP_NODELAY,&flag, sizeof(flag));
+	if (rc == -1) {
+		rc = errno;
+		LOG4CXX_WARN(logger,fmt::format(_("Open I/O port \"{0}\": setsockopt {1} error: errno = {2} : {3}"),
+				getPortName(),"TCP_NODELAY",rc,strerror(rc)));
+	}
 #endif // #if TCP_NODELAY
 #if TCP_QUICKACK
-		flag = 1;
-		rc = ::setsockopt(sock,IPPROTO_TCP,TCP_QUICKACK,&flag, sizeof(flag));
-		if (rc == -1) {
-			rc = errno;
-			LOG4CXX_WARN(logger,fmt::format(_("Open I/O port \"{0}\": setsockopt {1} error: errno = {2} : {3}"),
-					getPortName(),"TCP_QUICKACK",rc,strerror(rc)));
-		}
-#endif // #if TCP_QUICKACK
-
+	flag = 1;
+	rc = ::setsockopt(sock,IPPROTO_TCP,TCP_QUICKACK,&flag, sizeof(flag));
+	if (rc == -1) {
+		rc = errno;
+		LOG4CXX_WARN(logger,fmt::format(_("Open I/O port \"{0}\": setsockopt {1} error: errno = {2} : {3}"),
+				getPortName(),"TCP_QUICKACK",rc,strerror(rc)));
 	}
-
+#endif // #if TCP_QUICKACK
 
 }
 
