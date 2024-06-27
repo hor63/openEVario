b7db32e9446f2722fca40c51bc27f3bd377205da
Kai Horstmann <horstmannkai@hotmail.com>
2024-06-27 08:30:07 +0200

    Add class BusDeviceSensorBase and source files of the class
    BusDeviceSensorBase is virtual subclass of DeviceBase.
    It will implement the sensor thread function which does the error handling of
    sensors which share a port (i.e. I2C). That is substancially different
    than sensors which have dedicated ports/connections.

---
+++ src/main/driverBase/BusDeviceSensorBase.cpp
@@ -0,0 +1,39 @@
+/*
+ * BusDeviceSensorBase.cpp
+ *
+ *  Created on: Jun 26, 2024
+ *      Author: hor
+ *
+ *   This file is part of openEVario, an electronic variometer for glider planes
+ *   Copyright (C) 2022  Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include "main/driverBase/BusDeviceSensorBase.h"
+
+namespace openEV {
+namespace drivers {
+
+BusDeviceSensorBase::~BusDeviceSensorBase() {
+}
+
+BusDeviceSensorBase::BusDeviceSensorBase() {
+
+}
+
+} /* namespace drivers */
+} /* namespace openEV */
---
+++ src/main/driverBase/BusDeviceSensorBase.h
@@ -0,0 +1,44 @@
+/*
+ * BusDeviceSensorBase.h
+ *
+ *  Created on: Jun 26, 2024
+ *      Author: hor
+ *
+ *   This file is part of openEVario, an electronic variometer for glider planes
+ *   Copyright (C) 2022  Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef MAIN_DRIVERBASE_BUSDEVICESENSORBASE_H_
+#define MAIN_DRIVERBASE_BUSDEVICESENSORBASE_H_
+
+#include "drivers/DriverBase.h"
+
+namespace openEV {
+namespace drivers {
+
+class BusDeviceSensorBase: public virtual DriverBase {
+public:
+	BusDeviceSensorBase();
+
+	virtual ~BusDeviceSensorBase();
+};
+
+} /* namespace drivers */
+} /* namespace openEV */
+
+#endif /* MAIN_DRIVERBASE_BUSDEVICESENSORBASE_H_ */
